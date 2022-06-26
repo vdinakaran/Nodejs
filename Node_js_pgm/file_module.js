72be5cf1d19aff6df6535210cd3d9916778c18a0
varshini <varshini.dinakaran@capgemini.com>
2022-06-27 01:25:58 +0530

    file module with http

--- Node_js_pgm/file_module.js
+++ Node_js_pgm/file_module.js
@@ -1,12 +1,26 @@
 var fs = require("fs");
 //Synchronous
-var syn = fs.readFileSync("sample.txt");
-console.log("synchronous:" + syn.toString());
+// var syn = fs.readFileSync("sample.txt");
+// console.log("synchronous:" + syn.toString());
 
-//Asynchronous
-var asyn = fs.readFile("sample.txt", function (err, data) {
-  if (err) {
-    return console.log.error(err);
-  }
-  console.log("Asynchronous :" + data.toString());
-});
+// //Asynchronous
+// var asyn = fs.readFile("sample.txt", function (err, data) {
+//   if (err) {
+//     return console.log.error(err);
+//   }
+//   console.log("Asynchronous :" + data.toString());
+// });
+
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    var asyn = fs.readFile("sample.txt", function (err, data) {
+      if (err) {
+        return console.log.error(err);
+      }
+      console.log("Asynchronous :" + data.toString());
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(8001);
