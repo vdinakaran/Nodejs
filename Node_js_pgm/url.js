6c937ce66cf8b9b101a6579409a90f8494c97de0
varshini <varshini.dinakaran@capgemini.com>
2022-06-27 11:16:47 +0530

    changes committed

---
+++ Node_js_pgm/url.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+http
+  .createServer(function (req, res) {
+    var weblink = url.parse(req.url, true);
+    var filepath = "." + weblink.pathname;
+    var asyn = fs.readFile(filepath, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-type": "text/html" });
+
+        return res.end("404 file not found");
+      }
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(8010);
